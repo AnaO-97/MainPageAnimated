76db3158496c0491bc401fef501cb756b7c63e30
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-04-08 21:44:14 -0500

    Logic of operation

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function lines (){
-    let size = Math.random() * 22;
+    let sizeWidth = Math.random() * 22;
     let duration = Math.random() * 3;
 
     let e = document.createElement('div');
     e.setAttribute("class","circle");
+    document.body.appendChild(e);
+
+    e.style.width = 12 + sizeWidth + "px";
+    e.style.left  = Math.random()* + innerWidth + "px";
+    e.style.animation = 2 + duration + "s";
+
+    setTimeout(()=>{
+        document.body.removeChild(e);
+    }, 5000)
 }
+
+setInterval(() => {
+    lines();
+}, 200);
